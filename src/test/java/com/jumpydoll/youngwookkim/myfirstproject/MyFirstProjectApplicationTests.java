72dbe1cc5987befc9020867f770a34a03877cde2
young-wook-kim <youngwook.k@gmail.com>
2022-02-16 04:21:33 +0000

    Initial commit

---
+++ src/test/java/com/jumpydoll/youngwookkim/myfirstproject/MyFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpydoll.youngwookkim.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
